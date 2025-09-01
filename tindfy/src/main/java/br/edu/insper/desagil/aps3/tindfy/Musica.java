1d929798a898c155e89c3ecaf97b6cb1fcdb4082
ArthurTrottaCosta <166644298+ArthurTrottaCosta@users.noreply.github.com>
2025-09-01 16:35:24 -0300

    Update Musica.java

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Musica.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Musica.java
@@ -1,4 +1,20 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
 public class Musica {
+    private Artista artista;
+    private String titulo;
+
+    public Musica(Artista artista, String titulo) {
+        this.artista = artista;
+        this.titulo = titulo;
+    }
+
+    public Artista getArtista() {
+        return this.artista;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
 }
