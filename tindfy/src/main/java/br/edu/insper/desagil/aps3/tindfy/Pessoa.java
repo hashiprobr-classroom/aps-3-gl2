95740a033aaeac88505272459af10a7f03c9bc86
ArthurTrottaCosta <166644298+ArthurTrottaCosta@users.noreply.github.com>
2025-09-01 16:34:53 -0300

    Update Pessoa.java

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Pessoa.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Pessoa.java
@@ -1,4 +1,23 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
 public class Pessoa {
+    private int id;
+    public String nome;
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
