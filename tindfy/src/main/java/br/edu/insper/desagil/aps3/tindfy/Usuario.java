dbadd8e75b4dfc6f5f9c1eb0d8a11425973ba291
GuForbes <80713991+GuForbes@users.noreply.github.com>
2025-09-02 20:58:17 -0300

    Update Usuario.java

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
@@ -1,4 +1,44 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
-public class Usuario {
+import java.util.List;
+
+public class Usuario extends Pessoa{
+
+    private List<Musica> musicas;
+
+    public Usuario(Integer id, String nome, List<Musica> musicas) {
+        super(id, nome);
+        this.musicas = musicas;
+    }
+
+    public List<Musica> getMusicas() {return musicas;}
+
+
+    public Boolean jaAdicionou(Musica musica) {
+
+        for (Musica musicaTeste : musicas) {
+            if ((int) musica.getArtista().getId() == (int) (musicaTeste.getArtista().getId())) {
+                if (musica.getTitulo().equals(musicaTeste.getTitulo())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public void adiciona(Musica musica){
+            if (jaAdicionou(musica) == false){
+                musicas.add(musica);
+            }
+
+        }
+
+
+
+
+
+
+
+
 }
