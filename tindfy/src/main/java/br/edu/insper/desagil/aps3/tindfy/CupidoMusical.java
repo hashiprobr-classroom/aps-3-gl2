b113d3592bf6d904b65ec3834cfc1475589f5416
GuForbes <80713991+GuForbes@users.noreply.github.com>
2025-09-02 20:56:47 -0300

    Update CupidoMusical.java

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/CupidoMusical.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/CupidoMusical.java
@@ -1,4 +1,27 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
-public class CupidoMusical {
+import java.util.List;
+import java.util.Map;
+
+public class CupidoMusical extends Cupido {
+
+    public CupidoMusical(Map<Integer, List<Integer>> likes) {
+        super(likes);
+    }
+
+    @Override
+    public Boolean deuMatch(Usuario usuario1, Usuario usuario2) {
+        if (super.deuMatch(usuario1, usuario2)) {
+
+            List<Musica> musicas = usuario1.getMusicas();
+
+            for (Musica m1 : musicas) {
+                if(usuario2.jaAdicionou(m1)){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
 }
